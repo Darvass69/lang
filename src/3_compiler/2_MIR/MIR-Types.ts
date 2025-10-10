8cec27659d3a17c9aebe392c40af335428820c96
Darvass69 <86136459+Darvass69@users.noreply.github.com>
2025-10-10 00:02:31 -0400

    MIR visualization

--- src/3_compiler/2_MIR/MIR-Types.ts
+++ src/3_compiler/2_MIR/MIR-Types.ts
@@ -82,7 +82,7 @@ export function prettyPrintMIR(mir: Module): string {
   const linesMain: string[] = [];
 
   for (const fn of mir.functions) {
-    linesMain.push(`function ${fn.name === "" ? "$start$" : fn.name}(${printParams(fn.params)}) returns (${fn.returnType}) {`);
+    linesMain.push(`function ${fn.name}(${printParams(fn.params)}) returns (${fn.returnType}) {`);
     const linesFn = printFunction(fn).map((line) => "  " + line);
     linesMain.push(...linesFn);
     linesMain.push("}");
@@ -110,6 +110,12 @@ function printBlock(block: Block, visited: Set<Block>, lines: string[]) {
   // Block header
   lines.push(printLabel(block.name) + ":");
 
+  printInstructions(block, lines);
+
+  printTerminator(block, visited, lines);
+}
+
+function printInstructions(block: Block, lines: string[], trackFunctionCalls?: string[]) {
   // Instructions
   for (const instr of block.instructions) {
     switch (instr.type) {
@@ -125,14 +131,16 @@ function printBlock(block: Block, visited: Set<Block>, lines: string[]) {
         lines.push(`  push_param ${printOperand(instr.operand)}`);
         break;
       case "call":
-        lines.push(`  ${instr.dst && instr.dst.name + " = "}call ${instr.callee}(${instr.nbArgs})`);
+        lines.push(`  ${(instr.dst && instr.dst.name + " = ") ?? ""}call ${instr.callee}(${instr.nbArgs})`);
+        if (trackFunctionCalls) trackFunctionCalls.push(instr.callee);
         break;
       case "print":
         lines.push(`  print ${printOperand(instr.value)}`);
         break;
     }
   }
-
+}
+function printTerminator(block: Block, visited: Set<Block>, lines: string[]) {
   // Terminator
   if (block.terminator) {
     switch (block.terminator.type) {
@@ -153,12 +161,95 @@ function printBlock(block: Block, visited: Set<Block>, lines: string[]) {
         printBlock(block.terminator.end, visited, lines);
         break;
       case "return":
-        lines.push(`  return${block.terminator.value && " " + printOperand(block.terminator.value)}`);
+        lines.push(`  return${block.terminator.value ? " " + printOperand(block.terminator.value) : ""}`);
         break;
     }
   }
 }
 
 export function moduleToGraphView(module: Module): string {
-  return "";
+  var result = `digraph MIR {\n` +
+    `  graph [compound = true; fontsize = 12; splines = true;];\n` +
+    `  node [shape = box; fontname = "Courier New"; fontsize = 10;];\n` +
+    `  edge [fontname = "Courier New"; fontsize = 10;];\n`;
+
+  const trackFunctionCalls: [calling: string, called: string][] = [];
+  module.functions.forEach((fn) => result += graphFunction(fn, trackFunctionCalls));
+  [...new Map(trackFunctionCalls.map((pair) => [pair.join("|"), pair])).values()]
+    .forEach(([calling, called]) => result += `  "${calling}" -> "${called}::entry" [style=dashed label="call"];\n`);
+
+  result += "}";
+  return result;
+}
+
+function graphFunction(fn: Function, trackFunctionCalls: [calling: string, called: string][]): string {
+  let result = `  subgraph "cluster_${fn.name}" {\n` +
+    `    label = "${fn.name}";\n` +
+    `    color = "#cfe2ff";\n` +
+    `    style = "rounded,filled";\n`;
+
+  const visited = new Set<Block>();
+  const newTrackFunctionCalls: [blockName: string, callee: string][] = [];
+  result += graphBlock(fn.entry, fn.name, visited, newTrackFunctionCalls);
+
+  newTrackFunctionCalls.forEach(([blockName, callee]) => trackFunctionCalls.push([fn.name + "::" + blockName, callee]));
+
+  result += `    "${fn.name}#end" [label="end", shape=oval];\n`;
+
+  result += "  }\n";
+  return result;
+}
+
+function graphBlock(block: Block, fnName: string, visited: Set<Block>, trackFunctionCalls: [blockName: string, callee: string][]): string {
+  if (visited.has(block)) return "";
+  visited.add(block);
+
+  const newTrackFunctionCalls: string[] = [];
+  let result = `    "${fnName}::${block.name}" [label = "${graphInstructions(block, newTrackFunctionCalls)}"];\n`;
+  newTrackFunctionCalls.forEach((callee) => trackFunctionCalls.push([block.name, callee]));
+
+  result += graphTerminator(block, fnName, visited, trackFunctionCalls);
+
+  return result;
+}
+
+function graphInstructions(block: Block, trackFunctionCalls: string[]): string {
+  let lines: string[] = [];
+
+  printInstructions(block, lines, trackFunctionCalls);
+  lines = lines.map((line) => line.replace(/^  /, ""));
+
+  return lines.join("\\n");
+}
+
+function graphTerminator(block: Block, fnName: string, visited: Set<Block>, trackFunctionCalls: [blockName: string, callee: string][]): string {
+  let result = "";
+
+  if (block.terminator) {
+    switch (block.terminator.type) {
+      case "goto":
+        result += `    "${fnName}::${block.name}" -> "${fnName}::${block.terminator.target.name}";\n`;
+        result += graphBlock(block.terminator.target, fnName, visited, trackFunctionCalls);
+        break;
+      case "ifgoto":
+        result += `    "${fnName}::${block.name}" -> "${fnName}::${block.terminator.consequent.name}" [label = "if ${printOperand(block.terminator.test)}"];\n`;
+        result += graphBlock(block.terminator.consequent, fnName, visited, trackFunctionCalls);
+        result += `    "${fnName}::${block.name}" -> "${fnName}::${block.terminator.alternate.name}" [label = "else"];\n`;
+        result += graphBlock(block.terminator.alternate, fnName, visited, trackFunctionCalls);
+        break;
+      case "loop":
+        result += `    "${fnName}::${block.name}" -> "${fnName}::${block.terminator.body.name}" [label = "loop ${printOperand(block.terminator.test)}"];\n`;
+        result += graphBlock(block.terminator.body, fnName, visited, trackFunctionCalls);
+        result += `    "${fnName}::${block.name}" -> "${fnName}::${block.terminator.end.name}" [label = "end"];\n`;
+        result += graphBlock(block.terminator.end, fnName, visited, trackFunctionCalls);
+        break;
+      case "return":
+        result += `    "${fnName}::${block.name}" -> "${fnName}#end" [label = "return${block.terminator.value ? " " + printOperand(block.terminator.value) : ""}"];\n`;
+        break;
+    }
+  } else {
+    result += `    "${fnName}::${block.name}" -> "${fnName}#end";\n`;
+  }
+
+  return result;
 }
